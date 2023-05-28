29e4db330d7b2b0748cc6649a7dc5aa59e8873e2
Genius <lhadirahimil@gmail.com>
2023-05-28 19:58:43 +0330

    Initial commit

---
+++ app/src/main/java/com/hadirahimi/noteapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hadirahimi.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    //This section will be professionally developed using the
+    // MVVM architecture in the next programming section.
+    // Please subscribe and watch the next section inside the channel.
+    }
+    
+}
